5795098e7d44d05ce7503fda82249636d6e58936
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-03-30 20:13:24 -0700

    6. Adds home container

---
+++ src/containers/HomeContainer/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import { Redirect } from 'react-router-dom';
+
+const Home = () => {
+    state = {
+        specialty: "",
+        zipcode: "",
+        results: null,
+        errors: []
+    };
+    
+    return (
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Home;
